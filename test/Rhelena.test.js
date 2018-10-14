31b37788a828cd746648b5d1a930d98f95b7a86d
tiagostutz <tiagostutz@bb.com.br>
2018-10-14 01:48:41 -0300

    separating keepState from loadState for more control over the attribution flow

--- test/Rhelena.test.js
+++ test/Rhelena.test.js
@@ -19,14 +19,14 @@ describe('Rhelena', () => {
         })
     })
 
-    describe('#persist()', () => {
+    describe('#keepState()', () => {
         it('persist and retrieve the model directly', () => {
             const model_1 = new PersistentMockModel()
             assert.equal(model_1.__persistenceGroup, null)
             assert.equal(model_1.__persistenceId, null)
 
             //enable persistence
-            model_1.persist("mocks", 22)
+            model_1.keepState("mocks", 22)
             assert.equal(model_1.__persistenceGroup, "mocks")
             assert.equal(model_1.__persistenceId, 22)
 
@@ -40,7 +40,7 @@ describe('Rhelena', () => {
 
             //enable persistence
             model_1.value = 1
-            model_1.persist("mocks", 22) // first persist
+            model_1.keepState("mocks", 22) // first persist
             assert.equal(model_1.__persistenceGroup, "mocks")
             assert.equal(model_1.__persistenceId, 22)
             assert.equal(globalState['mocks'][22].value, 1)
@@ -73,15 +73,15 @@ describe('Rhelena', () => {
             assert.equal(model_1.__persistenceId, null)
             assert.equal(model_1.value, 300)
             
-            attachModelToView(model_1, new ViewMock()) //first attach
+            attachModelToView(model_1, new ViewMock()) //first, attach
             assert.equal(globalState['mocks'], undefined)
             assert.equal(model_1.value, 300)
 
-            model_1.persist("mocks", 33) //then persist
+            model_1.keepState("mocks", 33) //then, keepState
             assert.equal(model_1.__persistenceGroup, "mocks")
             assert.equal(model_1.__persistenceId, 33)
-            assert.equal(model_1.value, 300)
             assert.equal(globalState['mocks'][33].value, 300)
+            assert.equal(model_1.value, 300)
 
             model_1.value = 444
             assert.equal(model_1.value, 444)
@@ -102,7 +102,7 @@ describe('Rhelena', () => {
 
             //enable persistence
             model_1.value = 1
-            model_1.persist("mocks", 22) // first persist
+            model_1.keepState("mocks", 22) // first persist
             assert.equal(model_1.__persistenceGroup, "mocks")
             assert.equal(model_1.__persistenceId, 22)
             assert.equal(globalState['mocks'][22].value, 1)
@@ -122,7 +122,7 @@ describe('Rhelena', () => {
 
             const model_2 = new PersistentMockModel()
             assert.equal(model_2.value, null)
-            model_2.persist("mocks", 22) // first persist
+            model_2.loadState("mocks", 22) // first persist
             assert.equal(model_2.value, 7777)
 
             delete globalState['mocks']
@@ -135,17 +135,17 @@ describe('Rhelena', () => {
             assert.equal(model_1.__persistenceGroup, null)
             assert.equal(model_1.__persistenceId, null)
 
-            //enable persistence
+            // simple assignment
             model_1.value = 300
             assert.equal(model_1.__persistenceGroup, null)
             assert.equal(model_1.__persistenceId, null)
             assert.equal(model_1.value, 300)
             
-            attachModelToView(model_1, new ViewMock()) //first attach
+            attachModelToView(model_1, new ViewMock()) //first, attach
             assert.equal(globalState['mocks'], undefined)
             assert.equal(model_1.value, 300)
 
-            model_1.persist("mocks", 33) //then persist
+            model_1.keepState("mocks", 33) //then, keepState
             assert.equal(model_1.__persistenceGroup, "mocks")
             assert.equal(model_1.__persistenceId, 33)
             assert.equal(model_1.value, 300)
@@ -161,8 +161,148 @@ describe('Rhelena', () => {
 
             const model_2 = new PersistentMockModel()
             assert.equal(model_2.value, null)
-            model_2.persist("mocks", 33) // first persist
+            model_2.loadState("mocks", 33) // first persist
             assert.equal(model_2.value, 5555)
+
+            delete globalState['mocks']
+
+        })
+
+        it('attachToView, post-persist with differente IDs changing values and checking the model values', () => {
+            const model_1 = new PersistentMockModel()
+            assert.equal(globalState['mocks'], undefined)
+            assert.equal(model_1.__persistenceGroup, null)
+            assert.equal(model_1.__persistenceId, null)
+
+            // simple assignment
+            model_1.value = 300
+            assert.equal(globalState['mocks'], undefined)
+            assert.equal(model_1.__persistenceGroup, null)
+            assert.equal(model_1.__persistenceId, null)
+            assert.equal(model_1.value, 300)
+            
+            attachModelToView(model_1, new ViewMock()) //first, attach
+            assert.equal(globalState['mocks'], undefined)
+            assert.equal(model_1.__persistenceGroup, null)
+            assert.equal(model_1.__persistenceId, null)            
+            assert.equal(model_1.value, 300)
+
+            model_1.keepState("mocks", 33) //then, keepState
+            assert.equal(model_1.__persistenceGroup, "mocks")
+            assert.equal(model_1.__persistenceId, 33)
+            assert.equal(model_1.value, 300)
+            assert.equal(globalState['mocks'][33].value, 300)
+
+            model_1.value = 444
+            assert.equal(model_1.value, 444)
+            assert.equal(globalState['mocks'][33].value, 444)
+
+            globalState['mocks'][33].value = 5555
+            assert.equal(globalState['mocks'][33].value, 5555)
+            assert.equal(model_1.value, 5555)
+
+            model_1.keepState("mocks", 44)
+            assert.equal(model_1.value, 444)
+            assert.equal(globalState['mocks'][44].value, 444)
+            assert.equal(globalState['mocks'][33].value, 5555)
+            model_1.value = 5550
+            assert.equal(model_1.value, 5550)
+            assert.equal(globalState['mocks'][44].value, 5550)
+            assert.equal(globalState['mocks'][33].value, 5555)
+
+            model_1.loadState("mocks", 33)
+            assert.equal(model_1.value, 5555)
+            assert.equal(globalState['mocks'][44].value, 5550)
+            assert.equal(globalState['mocks'][33].value, 5555)
+
+            model_1.loadState("mocks", 44)
+            assert.equal(model_1.value, 5550)
+            assert.equal(globalState['mocks'][44].value, 5550)
+            assert.equal(globalState['mocks'][33].value, 5555)
+
+            delete globalState['mocks']
+        })
+
+        it('attachToView, post-persist with differente IDs changing values, checking if persisted and checking the model values', () => {
+
+            const model_1 = new PersistentMockModel()
+            assert.equal(globalState['mocks'], undefined)
+            assert.equal(model_1.__persistenceGroup, null)
+            assert.equal(model_1.__persistenceId, null)
+
+            //enable persistence
+            model_1.value = 100
+            assert.equal(globalState['mocks'], undefined)
+            assert.equal(model_1.__persistenceGroup, null)
+            assert.equal(model_1.__persistenceId, null)
+            assert.equal(model_1.value, 100)
+            
+            attachModelToView(model_1, new ViewMock()) //first, attach
+            assert.equal(globalState['mocks'], undefined)
+            assert.equal(model_1.__persistenceGroup, null)
+            assert.equal(model_1.__persistenceId, null)            
+            assert.equal(model_1.value, 100)
+
+            model_1.keepState("mocks", 33) //then, keepState
+            assert.equal(model_1.__persistenceGroup, "mocks")
+            assert.equal(model_1.__persistenceId, 33)
+            assert.equal(model_1.value, 100)
+            assert.equal(globalState['mocks'][33].value, 100)
+
+            if (!model_1.isStateKept("mocks", 55)) {
+                model_1.keepState("mocks", 55) //then, keepState
+                model_1.value = 200
+                assert.equal(model_1.__persistenceGroup, "mocks")
+                assert.equal(model_1.__persistenceId, 55)
+                assert.equal(model_1.value, 200)
+                assert.equal(globalState['mocks'][55].value, 200)
+                assert.equal(globalState['mocks'][33].value, 100)
+            }
+          
+            assert.equal(model_1.isStateKept("mocks", 33), !!true)
+            assert.equal(model_1.isStateKept("mocks", 55), !!true)
+
+            if (model_1.isStateKept("mocks", 33)) {
+                model_1.loadState("mocks", 33) //loadState `clears` the keepState
+                assert.equal(model_1.__persistenceGroup, null)
+                assert.equal(model_1.__persistenceId, null)
+                assert.equal(model_1.value, 100)
+                assert.equal(globalState['mocks'][55].value, 200)
+                assert.equal(globalState['mocks'][33].value, 100) 
+                model_1.value = 300               
+                assert.equal(model_1.value, 300)
+                assert.equal(globalState['mocks'][55].value, 200)
+                assert.equal(globalState['mocks'][33].value, 100) 
+                model_1.keepState("mocks", 33)
+                assert.equal(model_1.value, 300)
+                assert.equal(globalState['mocks'][55].value, 200)
+                assert.equal(globalState['mocks'][33].value, 300) 
+                model_1.value = 303               
+                assert.equal(model_1.value, 303)
+                assert.equal(globalState['mocks'][55].value, 200)
+                assert.equal(globalState['mocks'][33].value, 303) 
+            }
+
+            if (model_1.isStateKept("mocks", 55)) {
+                model_1.loadState("mocks", 55) //loadState `clears` the keepState
+                assert.equal(model_1.__persistenceGroup, null)
+                assert.equal(model_1.__persistenceId, null)
+                assert.equal(model_1.value, 200)
+                assert.equal(globalState['mocks'][55].value, 200)
+                assert.equal(globalState['mocks'][33].value, 303) 
+                model_1.value = 500               
+                assert.equal(model_1.value, 500)
+                assert.equal(globalState['mocks'][55].value, 200)
+                assert.equal(globalState['mocks'][33].value, 303) 
+                model_1.keepState("mocks", 55)
+                assert.equal(model_1.value, 500)
+                assert.equal(globalState['mocks'][55].value, 500)
+                assert.equal(globalState['mocks'][33].value, 303) 
+                model_1.value = 505               
+                assert.equal(model_1.value, 505)
+                assert.equal(globalState['mocks'][55].value, 505)
+                assert.equal(globalState['mocks'][33].value, 303) 
+            }
 
             delete globalState['mocks']
 
